d8eaaaa33836173de9dd1edf438d398aabea9f8e
Glyn Finck <glynfinck@Glyns-MacBook-Pro.local>
2025-06-16 19:50:16 -0700

    Adding.

--- kraken_trade_book_deploy.py
+++ kraken_trade_book_deploy.py
@@ -2,7 +2,8 @@ from prefect import flow
 from prefect.docker import DockerImage
 from prefect_github import GitHubCredentials
 from prefect.runner.storage import GitRepository
-
+from prefect.schedules import Interval
+from datetime import datetime
 
 if __name__ == "__main__":
     source = GitRepository(
@@ -19,6 +20,7 @@ if __name__ == "__main__":
         name="pull_kraken_trade_book_xbtusd",
         work_pool_name="kubernetes-default",
         parameters={"pairs": ["XBTUSD", "XBTEUR"], "count": 500},  # Default parameters
+        interval=Interval(interval=60, anchor_date=datetime(2000, 1, 1, 0, 0, 0)),
         build=False,
         push=False,
     )
