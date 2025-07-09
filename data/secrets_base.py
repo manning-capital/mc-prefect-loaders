f9fdbca4117ed35f15f177939f31716534c6eb11
Glyn Finck <glynfinck@Glyns-MacBook-Pro.local>
2025-07-09 23:00:05 +0100

    Fixing.

---
+++ data/secrets_base.py
@@ -0,0 +1,7 @@
+import os
+from prefect.blocks.system import Secret
+
+if __name__ == "__main__":
+    Secret(value=os.getenv("PREFECT_GITHUB_CREDENTIALS")).save(
+        "github-credentials", overwrite=True
+    )
