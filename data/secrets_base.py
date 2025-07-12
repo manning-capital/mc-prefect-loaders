43bbfb9f1650d5e1e4b4e471b3f0292d4f9932ec
Glyn Finck <glynfinck@Glyns-MacBook-Pro.local>
2025-07-12 11:13:02 +0100

    Adding postgres password.

--- data/secrets_base.py
+++ data/secrets_base.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     GitHubCredentials(token=os.getenv("PREFECT_GITHUB_CREDENTIALS")).save(
         "github-credentials", overwrite=True
     )
+    Secret(value=os.getenv("POSTGRESQL_PASSWORD")).save(
+        "postgresql-password", overwrite=True
+    )
